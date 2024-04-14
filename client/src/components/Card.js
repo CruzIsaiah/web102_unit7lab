f67cae51a48882b719df033b0eebbd077249c48a
CruzIsaiah <isaiahcruz13@yahoo.com>
2024-04-14 13:25:13 -0400

    Added functionality to pass characterImage as URL parameter

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,41 +1,46 @@
-import React, { useState } from 'react';
-import './Card.css';
-import more from './more.png';
-import { Link } from 'react-router-dom';
-import { supabase } from '../client'; // Import the Supabase client
+import React, { useState } from "react";
+import "./Card.css";
+import more from "./more.png";
+import { Link } from "react-router-dom";
+import { supabase } from "../client"; // Import the Supabase client
 
 const Card = (props) => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    const updateCount = async () => {
-        try {
-            // Update bet count in the database
-            await supabase
-                .from('Posts')
-                .update({ betCount: count + 1 })
-                .eq('id', props.id);
+  const updateCount = async () => {
+    try {
+      // Update bet count in the database
+      await supabase
+        .from("Posts")
+        .update({ betCount: count + 1 })
+        .eq("id", props.id);
 
-            // Update the bet count in the UI
-            setCount((count) => count + 1);
-        } catch (error) {
-            console.error('Error updating bet count:', error.message);
-            // Handle error, e.g., display error message to the user
-        }
-    };
+      // Update the bet count in the UI
+      setCount((count) => count + 1);
+    } catch (error) {
+      console.error("Error updating bet count:", error.message);
+      // Handle error, e.g., display error message to the user
+    }
+  };
 
-    return (
-        <div className="Card">
-            <Link to={'edit/' + props.id}>
-                <img className="moreButton" alt="edit button" src={more} />
-            </Link>
-            <h2 className="title">{props.title}</h2>
-            <h3 className="author">{"by " + props.author}</h3>
-            <p className="description">{props.description}</p>
-            <button className="betButton" onClick={updateCount}>
-                👍 Bet Count: {count}
-            </button>
-        </div>
-    );
+  return (
+    <div className="Card">
+      <Link to={"edit/" + props.id}>
+        <img className="moreButton" alt="edit button" src={more} />
+      </Link>
+      <h2 className="title">{props.title}</h2>
+      <h3 className="author">{"by " + props.author}</h3>
+      <img
+        className="characterImage"
+        src={props.characterImage}
+        alt="Character"
+      />
+      <p className="description">{props.description}</p>
+      <button className="betButton" onClick={updateCount}>
+        👍 Bet Count: {count}
+      </button>
+    </div>
+  );
 };
 
 export default Card;
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,51 +1,168 @@
-import React, { useState } from 'react';
-import { supabase } from '../client';
+import React, { useState } from "react";
+import { supabase } from "../client";
 
 const CreatePost = () => {
-    const [post, setPost] = useState({ title: "", author: "", description: "" });
+  const [post, setPost] = useState({
+    title: "",
+    author: "",
+    description: "",
+    characterImage: "",
+    intelligence: 0,
+    perception: 0,
+    agility: 0,
+    charisma: 0,
+    endurance: 0,
+    luck: 0,
+    strength: 0,
+    fastShot: 0,
+    heavyHanded: 0,
+  });
+  const totalPoints = 10; // Total points available to distribute
+  const attributes = [
+    "intelligence",
+    "perception",
+    "agility",
+    "charisma",
+    "endurance",
+    "luck",
+    "strength",
+    "fastShot",
+    "heavyHanded",
+  ];
 
-    const createPost = async (event) => {
-        event.preventDefault();
+  const characterData = {
+    Select: "",
+    Mario: "mario.png",
+    Luigi: "Lu.jpg",
+    Peach: "peach.png",
+    Bowser: "bowser.png",
+    DK: "dk.png",
+    Wario: "wario.jpg",
+    WaLuigi: "waluigi.png",
+    Fox: "fox.png",
+    Ness: "ness.png",
+    Lucas: "lucas.png",
+    Falcon: "cap.png",
+    // Add more character names and their corresponding image URLs here
+  };
 
-        try {
-            await supabase
-                .from('Posts')
-                .insert({ title: post.title, author: post.author, description: post.description })
-                .select();
+  const createPost = async (event) => {
+    event.preventDefault();
 
-            window.location = "/";
-        } catch (error) {
-            console.error('Error creating post:', error.message);
-            // Handle error, e.g., display error message to the user
-        }
-    };
+    try {
+      await supabase.from("Posts").insert(post).select();
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setPost((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
+      window.location = "/";
+    } catch (error) {
+      console.error("Error creating post:", error.message);
+      // Handle error, e.g., display error message to the user
+    }
+  };
 
-    return (
-        <div>
-            <form>
-                <label htmlFor="title">Title</label> <br />
-                <input type="text" id="title" name="title" value={post.title} onChange={handleChange} /><br />
-                <br />
+  const handleCharacterChange = (event) => {
+    const characterName = event.target.value;
+    setPost((prev) => ({
+      ...prev,
+      title: characterName,
+      characterImage: characterData[characterName] || "", // Get the corresponding image URL based on the selected character name
+    }));
+  };
 
-                <label htmlFor="author">Author</label><br />
-                <input type="text" id="author" name="author" value={post.author} onChange={handleChange} /><br />
-                <br />
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-                <label htmlFor="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" name="description" value={post.description} onChange={handleChange}></textarea>
-                <br />
-                <input type="submit" value="Submit" onClick={createPost} />
-            </form>
-        </div>
-    );
+  const handleAttributeChange = (attribute, value) => {
+    const remainingPoints =
+      totalPoints -
+      Object.values(post).reduce((acc, val) => acc + val, 0) +
+      post[attribute] -
+      value;
+    if (value <= totalPoints && value >= 0) {
+      setPost((prev) => ({
+        ...prev,
+        [attribute]: value,
+      }));
+    } else {
+      alert(`Please allocate points between 0 and ${totalPoints}.`);
+    }
+    // eslint-disable-next-line
+    console.log("Remaining points:", remainingPoints);
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="title">Character Model</label> <br />
+        {post.characterImage && (
+          <div>
+            <img
+              src={post.characterImage}
+              alt="Character"
+              style={{ width: "100px", height: "100px" }}
+            />
+          </div>
+        )}
+        <select id="title" name="title" onChange={handleCharacterChange}>
+          <option value="">Select Character</option>
+          {Object.keys(characterData).map((name, index) => (
+            <option key={index} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+        <label htmlFor="author">Character Name</label>
+        <br />
+        <input
+          type="text"
+          id="author"
+          name="author"
+          value={post.author}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label htmlFor="description">Character Description</label>
+        <br />
+        <textarea
+          rows="5"
+          cols="50"
+          id="description"
+          name="description"
+          value={post.description}
+          onChange={handleChange}
+        ></textarea>
+        <br />
+        <br />
+        <label>Allocate Points (Total: {totalPoints})</label>
+        <br />
+        {attributes.map((attr, index) => (
+          <div key={index}>
+            <label htmlFor={attr}>{attr}</label>
+            <input
+              type="number"
+              id={attr}
+              name={attr}
+              value={post[attr]}
+              min="0"
+              max={totalPoints}
+              onChange={(e) =>
+                handleAttributeChange(attr, parseInt(e.target.value))
+              }
+            />
+          </div>
+        ))}
+        <br />
+        <input type="submit" value="Submit" onClick={createPost} />
+      </form>
+    </div>
+  );
 };
 
 export default CreatePost;
